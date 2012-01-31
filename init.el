8ff009d4586ef8207c556c3283b80fcd67a6b29e
Joe Gallo <joegallo@gmail.com>
2012-01-31 16:30:01 -0500

    super sweet magit highlighting changes

--- init.el
+++ init.el
@@ -26,6 +26,13 @@
 (when (>= (display-color-cells) 256)
   (color-theme-twilight))
 
+(eval-after-load 'magit
+  '(progn
+     (set-face-foreground 'magit-diff-add "green3")
+     (set-face-foreground 'magit-diff-del "red3")
+     (when (not window-system)
+       (set-face-background 'magit-item-highlight "gray13"))))
+
 ;; === no back up files ===
 (setq make-backup-files nil)
 (setq auto-save-default nil)

5eb5f59fed797542b17b83776fa34dff3b7009ed
Joe Gallo <joegallo@gmail.com>
2013-09-10 13:48:12 -0400

    mega awesome dir tracking
    (maybe only logical on linux)

--- init.el
+++ init.el
@@ -77,6 +77,21 @@
   (setq comint-process-echoes t))
 (add-hook 'comint-mode-hook 'comint-stfu)
 
+;; === shell dir-tracking ===
+(defun track-shell-directory/procfs ()
+  (shell-dirtrack-mode 0)
+  (add-hook 'comint-preoutput-filter-functions
+            (lambda (str)
+              (prog1 str
+                (when (string-match comint-prompt-regexp str)
+                  (cd (file-symlink-p
+                       (format "/proc/%s/cwd" (process-id
+                                               (get-buffer-process
+                                                (current-buffer)))))))))
+            nil t))
+
+(add-hook 'shell-mode-hook 'track-shell-directory/procfs)
+
 ;; === ssh agent fix ===
 (defun agent-path ()
   (if (eq system-type 'darwin)
